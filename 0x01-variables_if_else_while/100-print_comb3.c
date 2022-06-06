e14d6a61a8fe49c6139b0d6348ad0ccc782f3787
Feujio <mtfeujio@gmail.com>
2022-06-06 02:48:10 -0700

    Return correct output and betty coding style OK

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,7 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if ( i == 0)
-			j = 0;
-		else
-			j = i + 1;
-		for (; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
