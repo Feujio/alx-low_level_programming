4ce1d38f46e58f273df336a629fa1929bfaa6929
Feujio <mtfeujio@gmail.com>
2022-06-08 09:22:19 -0700

    fix: return correct output and correct formatting

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -7,12 +7,18 @@
  */
 int main(void)
 {
-    /*print_times_table(3);
+    print_times_table(3);
     _putchar('\n');
     print_times_table(5);
-    _putchar('\n');*/
-    print_times_table(98);
     _putchar('\n');
+    print_times_table(10);
+    _putchar('\n');
+    print_times_table(0);
+    _putchar('\n');
+    print_times_table(13);
+    _putchar('\n');
+   /*print_times_table(98);
+    _putchar('\n');*/
     /*print_times_table(12);  */
     return (0);
 }
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,42 +11,55 @@ void print_times_table(int n)
 {
 	int i, j, x, first_digit_x, last_digit_x;
 
-	for (i = 0; i <= n; i++)
+	if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		for (j = 1; j <= n; j++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
 		{
-			if (i == 0)
+			_putchar('0');
+			_putchar(',');
+			for (j = 1; j <= n; j++)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0');
-				if (j == n)
-					continue;
-				_putchar(',');
-			} else
-			{
-				x = i * j;
-				if (x < 100)
+				if (i == 0)
 				{
 					_putchar(' ');
-					last_digit_x = x % 10;
-					first_digit_x = x / 10;
-					if (!first_digit_x)
-						_putchar(' ');
-					else
-						_putchar(first_digit_x + '0');
-					_putchar(last_digit_x + '0');
-				} else if (x >= 100)
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0');
+					if (j == n)
+						continue;
+					_putchar(',');
+				} else
 				{
-					printf("%d", x);
+					x = i * j;
+					if (x < 100)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						last_digit_x = x % 10;
+						first_digit_x = x / 10;
+						if (!first_digit_x)
+							_putchar(' ');
+						else
+							_putchar(first_digit_x + '0');
+						_putchar(last_digit_x + '0');
+					} else
+					{
+						_putchar(' ');
+						_putchar((x / 100) + '0');
+						_putchar(((x / 10) % 10) + '0');
+						_putchar((x % 10) + '0');
+					}
+					if (j == n)
+						continue;
+					_putchar(',');
 				}
-				if (j == n)
-					continue;
-				_putchar(',');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
